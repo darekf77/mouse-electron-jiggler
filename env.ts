21fd8d4654c482c7ed7dd2df11b87e7e64ab8a28
Dariusz Filipiak <darekf77@gmail.com>
2025-05-22 13:53:37 +0200

    first

---
+++ env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'mouse-electron-jiggler.example.domain.com',
+    title: 'Mouse Electron Jiggler',
+  },
+};
+export default env;
