700cce5e17d9e1cdc6371c9325b4c456d391fa55
Dariusz Filipiak <darekf77@gmail.com>
2025-06-16 20:22:26 +0200

    chore: update

--- environments/electron-app/env.electron-app.__.ts
+++ environments/electron-app/env.electron-app.__.ts
@@ -3,5 +3,9 @@ import baseEnv from '../../env';
 
 const env: Partial<EnvOptions> = {
   ...baseEnv,
+  release: {
+    staticPagesCustomRepoUrl:
+      'git@github.com:darekf77/mouse-electron-jiggler-versions.git',
+  },
 };
 export default env;
